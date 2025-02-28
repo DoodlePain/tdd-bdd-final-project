0a34f5c620a3ffdf2bfbe58f8fdbdcf11cef7296
DoodlePain <doodelinho@gmail.com>
2025-02-28 15:52:52 +0100

    FInal

--- features/products.feature
+++ features/products.feature
@@ -38,3 +38,95 @@ Scenario: Create a Product
     And I should see "True" in the "Available" dropdown
     And I should see "Tools" in the "Category" dropdown
     And I should see "34.95" in the "Price" field
+
+Scenario: Update a Product
+    Given the following products
+        | name       | description     | price   | available | category   |
+        | Hat        | A red fedora    | 59.95   | True      | CLOTHS     |
+    When I visit the "Home Page"
+    And I set the "Name" to "Hat"
+    And I press the "Search" button
+    Then I should see the message "Success"
+    And I should see "Hat" in the "Name" field
+    And I set the "Description" to "Updated red fedora"
+    And I press the "Update" button
+    Then I should see the message "Success"
+    When I press the "Retrieve" button
+    Then I should see "Updated red fedora" in the "Description" field
+
+Scenario: Delete a Product
+    Given the following products
+        | name       | description     | price   | available | category   |
+        | Hat        | A red fedora    | 59.95   | True      | CLOTHS     |
+    When I visit the "Home Page"
+    And I set the "Name" to "Hat"
+    And I press the "Search" button
+    Then I should see the message "Success"
+    And I should see "Hat" in the "Name" field
+    When I press the "Delete" button
+    Then I should see the message "Success"
+    When I press the "Retrieve" button
+    Then I should see the message "404 Not Found"
+
+Scenario: List all Products
+    Given the following products
+        | name       | description     | price   | available | category   |
+        | Hat        | A red fedora    | 59.95   | True      | CLOTHS     |
+        | Shoes      | Blue shoes      | 120.50  | False     | CLOTHS     |
+        | Big Mac    | 1/4 lb burger   | 5.99    | True      | FOOD       |
+        | Sheets     | Full bed sheets | 87.00   | True      | HOUSEWARES |
+    When I visit the "Home Page"
+    And I press the "List All" button
+    Then I should see the message "Success"
+    And I should see "Hat" in the results
+    And I should see "Shoes" in the results
+    And I should see "Big Mac" in the results
+    And I should see "Sheets" in the results
+
+Scenario: Search by category
+    Given the following products
+        | name       | description     | price   | available | category   |
+        | Hat        | A red fedora    | 59.95   | True      | CLOTHS     |
+        | Shoes      | Blue shoes      | 120.50  | False     | CLOTHS     |
+        | Big Mac    | 1/4 lb burger   | 5.99    | True      | FOOD       |
+        | Sheets     | Full bed sheets | 87.00   | True      | HOUSEWARES |
+    When I visit the "Home Page"
+    And I press the "Clear" button
+    And I select "CLOTHS" in the "Category" dropdown
+    And I press the "Search" button
+    Then I should see the message "Success"
+    And I should see "Hat" in the results
+    And I should see "Shoes" in the results
+    And I should not see "Big Mac" in the results
+    And I should not see "Sheets" in the results
+
+Scenario: Search by available
+    Given the following products
+        | name       | description     | price   | available | category   |
+        | Hat        | A red fedora    | 59.95   | True      | CLOTHS     |
+        | Shoes      | Blue shoes      | 120.50  | False     | CLOTHS     |
+        | Big Mac    | 1/4 lb burger   | 5.99    | True      | FOOD       |
+        | Sheets     | Full bed sheets | 87.00   | True      | HOUSEWARES |
+    When I visit the "Home Page"
+    And I press the "Clear" button
+    And I select "True" in the "Available" dropdown
+    And I press the "Search" button
+    Then I should see the message "Success"
+    And I should see "Hat" in the results
+    And I should see "Big Mac" in the results
+    And I should see "Sheets" in the results
+    And I should not see "Shoes" in the results
+
+Scenario: Search by name
+    Given the following products
+        | name       | description     | price   | available | category   |
+        | Hat        | A red fedora    | 59.95   | True      | CLOTHS     |
+        | Shoes      | Blue shoes      | 120.50  | False     | CLOTHS     |
+        | Big Mac    | 1/4 lb burger   | 5.99    | True      | FOOD       |
+        | Sheets     | Full bed sheets | 87.00   | True      | HOUSEWARES |
+    When I visit the "Home Page"
+    And I set the "Name" to "Hat"
+    And I press the "Search" button
+    Then I should see the message "Success"
+    And I should see "Hat" in the "Name" field
+    And I should see "A red fedora" in the "Description" field
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,28 +1,5 @@
-# Copyright 2016, 2023 John J. Rofrano. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# https://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# tests/test_models.py
 
-"""
-Test cases for Product Model
-
-Test cases can be run with:
-    nosetests
-    coverage report -m
-
-While debugging just these tests it's convenient to use this:
-    nosetests --stop tests/test_models.py:TestProductModel
-
-"""
 import os
 import logging
 import unittest
@@ -35,11 +12,6 @@ DATABASE_URI = os.getenv(
     "DATABASE_URI", "postgresql://postgres:postgres@localhost:5432/postgres"
 )
 
-
-######################################################################
-#  P R O D U C T   M O D E L   T E S T   C A S E S
-######################################################################
-# pylint: disable=too-many-public-methods
 class TestProductModel(unittest.TestCase):
     """Test Cases for Product Model"""
 
@@ -66,10 +38,6 @@ class TestProductModel(unittest.TestCase):
         """This runs after each test"""
         db.session.remove()
 
-    ######################################################################
-    #  T E S T   C A S E S
-    ######################################################################
-
     def test_create_a_product(self):
         """It should Create a product and assert that it exists"""
         product = Product(name="Fedora", description="A red hat", price=12.50, available=True, category=Category.CLOTHS)
@@ -89,11 +57,9 @@ class TestProductModel(unittest.TestCase):
         product = ProductFactory()
         product.id = None
         product.create()
-        # Assert that it was assigned an id and shows up in the database
         self.assertIsNotNone(product.id)
         products = Product.all()
         self.assertEqual(len(products), 1)
-        # Check that it matches the original product
         new_product = products[0]
         self.assertEqual(new_product.name, product.name)
         self.assertEqual(new_product.description, product.description)
@@ -101,6 +67,91 @@ class TestProductModel(unittest.TestCase):
         self.assertEqual(new_product.available, product.available)
         self.assertEqual(new_product.category, product.category)
 
-    #
-    # ADD YOUR TEST CASES HERE
-    #
+    def test_read_a_product(self):
+        """It should Read a product"""
+        product = ProductFactory()
+        product.id = None
+        product.create()
+        self.assertIsNotNone(product.id)
+        found_product = Product.find(product.id)
+        self.assertIsNotNone(found_product)
+        self.assertEqual(found_product.id, product.id)
+        self.assertEqual(found_product.name, product.name)
+        self.assertEqual(found_product.description, product.description)
+        self.assertEqual(found_product.price, product.price)
+        self.assertEqual(found_product.available, product.available)
+        self.assertEqual(found_product.category, product.category)
+
+    def test_update_a_product(self):
+        """It should Update a product"""
+        product = ProductFactory()
+        product.id = None
+        product.create()
+        self.assertIsNotNone(product.id)
+        product.description = "Updated description"
+        product.update()
+        updated_product = Product.find(product.id)
+        self.assertEqual(updated_product.description, "Updated description")
+
+    def test_delete_a_product(self):
+        """It should Delete a product"""
+        product = ProductFactory()
+        product.id = None
+        product.create()
+        self.assertIsNotNone(product.id)
+        product.delete()
+        deleted_product = Product.find(product.id)
+        self.assertIsNone(deleted_product)
+
+    def test_list_all_products(self):
+        """It should List all products"""
+        products = Product.all()
+        self.assertEqual(products, [])
+        for _ in range(5):
+            product = ProductFactory()
+            product.id = None
+            product.create()
+        products = Product.all()
+        self.assertEqual(len(products), 5)
+
+    def test_find_product_by_name(self):
+        """It should Find a product by name"""
+        products = []
+        for _ in range(5):
+            product = ProductFactory()
+            product.id = None
+            product.create()
+            products.append(product)
+        name = products[0].name
+        found_products = Product.find_by_name(name)
+        self.assertEqual(len(found_products), len([p for p in products if p.name == name]))
+        for product in found_products:
+            self.assertEqual(product.name, name)
+
+    def test_find_product_by_availability(self):
+        """It should Find a product by availability"""
+        products = []
+        for _ in range(10):
+            product = ProductFactory()
+            product.id = None
+            product.create()
+            products.append(product)
+        available = products[0].available
+        found_products = Product.find_by_availability(available)
+        self.assertEqual(len(found_products), len([p for p in products if p.available == available]))
+        for product in found_products:
+            self.assertEqual(product.available, available)
+
+    def test_find_product_by_category(self):
+        """It should Find a product by category"""
+        products = []
+        for _ in range(10):
+            product = ProductFactory()
+            product.id = None
+            product.create()
+            products.append(product)
+        category = products[0].category
+        found_products = Product.find_by_category(category)
+        self.assertEqual(len(found_products), len([p for p in products if p.category == category]))
+        for product in found_products:
+            self.assertEqual(product.category, category)
